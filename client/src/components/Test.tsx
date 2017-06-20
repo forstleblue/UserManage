b2ea48f65eb1274b8d81f6619d5c952fe279a4f8
Erwan Le Poder <e.lepoder@thecodingmachine.com>
2017-06-20 12:19:53 +0200

    First commit

---
+++ client/src/components/Test.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+export class Test extends React.Component<any, any> {
+
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+
+  render() {
+
+    return (
+      <h1>HELLO WORLD</h1>
+    );
+  }
+}
